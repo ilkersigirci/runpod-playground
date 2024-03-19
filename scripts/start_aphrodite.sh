17d66508df985271ff03705f60dace5b80665f93
ilkersigirci <sigirci.ilker@gmail.com>
2024-03-19 11:02:16 +0300

    aphrodite-engine version fixed

--- scripts/start_aphrodite.sh
+++ scripts/start_aphrodite.sh
@@ -8,13 +8,18 @@ if pip show torch | grep -q "Version: 2.1.1"; then
     pip uninstall torch torchaudio torchvision -y
 fi
 
+
 if ! pip show aphrodite-engine >/dev/null 2>&1; then
-    pip install aphrodite-engine
+    # pip install aphrodite-engine==0.5.2
+    curl -LR https://github.com/PygmalionAI/aphrodite-engine/releases/download/v0.5.2/aphrodite_engine-0.5.2-cp310-cp310-manylinux1_x86_64.whl --output "aphrodite_engine-0.5.2-cp310-cp310-manylinux1_x86_64.whl"
+    pip install aphrodite_engine-0.5.2-cp310-cp310-manylinux1_x86_64.whl
 fi
 
 # python -m aphrodite.endpoints.openai.api_server --help
 MODEL=/workspace/models/Mixtral-8x7B-Instruct-v0.1 && \
 MODEL_NAME=Mixtral-8x7B-Instruct-v0.1 && \
+# MODEL=/workspace/models/c4ai-command-r-v01 && \
+# MODEL_NAME=c4ai-command-r-v01 && \
 HF_HOME=/workspace/huggingface \
     python -m aphrodite.endpoints.openai.api_server \
     --host 0.0.0.0 \
