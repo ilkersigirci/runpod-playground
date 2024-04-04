0cb198087a3fb7244baed681e32cd17f20b4a441
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2024-04-04 16:37:33 +0300

    example notebooks added && Readme updated

--- README.md
+++ README.md
@@ -14,3 +14,37 @@ HF_HOME=/workspace/runpod-playground/huggingface python /workspace/runpod-playgr
 # Start vllm
 nohup bash /workspace/runpod-playground/scripts/start_vllm.sh > mixtral_vllm.txt &
 ```
+
+- Example curl
+
+```bash
+curl --request POST \
+    --url https://t7b5rytnikjr54-8000.proxy.runpod.net/v1/chat/completions \
+    --header "Content-Type: application/json" \
+    --data '{
+  "model": "Mixtral-8x7B-Instruct-v0.1",
+  "messages": [
+  {
+      "role": "system",
+      "content": "You are a helpful virtual assistant trained by OpenAI."
+  },
+  {
+    "role": "user",
+    "content": "Who are you?"
+  }
+  ], 
+  "temperature": 0.8,
+  "stream": false
+}'
+```
+
+## TabbyAPI Prompt Templates
+
+- [Official tabbyAPI Templates](https://github.com/theroyallab/llm-prompt-templates/)
+- [chat_templates github](https://github.com/chujiezheng/chat_templates)
+
+## Model CLI Download
+
+```bash
+huggingface-cli download turboderp/Llama2-7B-chat-exl2 --revision 4.0bpw --local-dir-use-symlinks False --local-dir my_model_dir
+```
--- scripts/start_tabbyapi.sh
+++ scripts/start_tabbyapi.sh
@@ -8,13 +8,28 @@ if pip show torch | grep -q "Version: 2.1.1"; then
     pip uninstall torch torchaudio torchvision -y
 fi
 
-# Symbolic link models
+cd /workspace/runpod-playground/tabbyAPI
+git clone https://github.com/theroyallab/tabbyAPI official-repo
+
+
+# Symbolic link config
+ln -s /workspace/runpod-playground/tabbyAPI/config.yml /workspace/runpod-playground/tabbyAPI/official-repo/config.yml
+
+## Sample Overrides
+ln -s /workspace/runpod-playground/tabbyAPI/sample_overrides.yml /workspace/runpod-playground/tabbyAPI/official-repo/sampler_overrides/sample_overrides.yml
+
+# Symbolic link Models
 for file in /workspace/runpod-playground/models/*; do
-    ln -s "$file" "/workspace/tabbyAPI/official-repo/models/$(basename "$file")"
+    ln -s "$file" "/workspace/runpod-playground/tabbyAPI/official-repo/models/$(basename "$file")"
+done
+
+## Symbolic link Prompts
+for file in /workspace/runpod-playground/tabbyAPI/prompt_templates/*; do
+    ln -s "$file" "/workspace/runpod-playground/tabbyAPI/official-repo/templates/$(basename "$file")"
 done
 
 if ! pip show exllamav2 >/dev/null 2>&1; then
-    pip install -r /workspace/tabbyAPI/official-repo/requirements.txt
+    pip install -r /workspace/runpod-playground/tabbyAPI/official-repo/requirements.txt
 fi
 
-cd /workspace/tabbyAPI/official-repo && python3 main.py
+cd /workspace/runpod-playground/tabbyAPI/official-repo && python3 main.py
